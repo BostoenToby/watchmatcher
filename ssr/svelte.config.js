ea7d1f82c9e08bd6a20beb7e5ac9b57f5c16924f
BostoenToby <toby.bostoen@student.howest.be>
2023-01-23 10:45:40 +0100

    🔍️feat: updating CSP for SSR

--- ssr/svelte.config.js
+++ ssr/svelte.config.js
@@ -12,8 +12,19 @@ const config = {
     adapter: adapter(),
     csp: {
       mode: 'hash',
-      directives: {"script-src": ["self"]}
-    }
+      directives: {
+        'script-src': ['self'],
+        'base-uri': ['self'],
+        'font-src': [
+          'self',
+          'https://fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-.ttf',
+          'https://fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Ew-.ttf',
+          'https://fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM70w-.ttf',
+          'https://fonts.gstatic.com/s/quattrocento/v17/OZpEg_xvsDZQL_LKIF7q4jPHxA.ttf',
+        ],
+        'script-src': ['self'],
+      },
+    },
   },
 }
 
