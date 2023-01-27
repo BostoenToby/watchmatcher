3dfec782adf2ea4c19bb042617f1b0398f9d4428
BostoenToby <71939691+BostoenToby@users.noreply.github.com>
2023-01-27 18:43:05 +0100

    Dev (#13)
    * 🎨feat: moved code to server side SSR
    
    * 🐛fix: possible fix for prerendering slug watches
    
    * Bugfixes SPA (#10)
    
    * 🐛fix: possible fix for length problem SPA
    
    * 🐛fix: possible fix for length problem spa
    
    * 🐛fix: possible fix for length problem SPA
    
    * 🐛fix: logging for problem SPA
    
    * 🐛fix: possible fix for length problem SPA
    
    * 🐛fix: bug is fixed for length problem SPA
    
    * ⚡️feat: added interface for errors
    
    * ⚡️feat: performance fix for SSG with interfaces
    
    * 📱feat: fixed header UX
    
    * 🐛fix: watches fix for SPA
    
    * 🐛fix: bugfix for length watches SPA
    
    * ✏️typo; typo fix
    
    * 🐛fix: bug fixed from merge

--- ssr/src/routes/brands/+page.svelte
+++ ssr/src/routes/brands/+page.svelte
@@ -9,9 +9,6 @@
   /** @type {import('./$types').PageData} */
   export let data: Images
 
-  /** @type {import('./$types').PageData} */
-  export let data: any
-
   const searchInput = writable<string>()
   const filteredBrands = writable<Array<Brand>>([])
 
